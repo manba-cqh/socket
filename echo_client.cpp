b73d5aa1477dfa713a4227a50a185576e9c7ca56
cuiqinghua <1131205877@qq.com>
2023-03-25 18:47:49 +0800

    添加文件

---
+++ echo_client.cpp
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#define BUFFER_LEN 1024
+
+int main(int argc, char *argv[])
+{
+    if (argc < 3) {
+        printf("please input ip and port!\n");
+        return 0;
+    }
+
+    int client_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (client_sock == -1) {
+        printf("socket error!\n");
+        return -1;
+    }
+
+    struct sockaddr_in server_addr;
+    memset(&server_addr, sizeof(server_addr), 0);
+    server_addr.sin_family=AF_INET;
+	server_addr.sin_addr.s_addr=inet_addr(argv[1]);
+	server_addr.sin_port=htons(atoi(argv[2]));
+
+    socklen_t server_addr_len = sizeof(server_addr);
+    if (connect(client_sock, (struct sockaddr*)&server_addr, server_addr_len) == -1) {
+        printf("connect error!\n");
+        return -1;
+    }
+
+    char message[BUFFER_LEN];
+    //fgets(message, BUFFER_LEN, stdin);
+    int str_len = 0;
+    int recv_len=0;
+    while(1) {
+        printf("please input send message:");
+        fgets(message, BUFFER_LEN, stdin);
+		
+		if(!strcmp(message,"q\n") || !strcmp(message,"Q\n"))
+			break;
+
+		str_len=write(client_sock, message, strlen(message));
+        if (str_len == -1) {
+            printf("write error!\n");
+            return -1;
+        }
+		
+		while(recv_len<str_len)
+		{
+			int recv_cnt=read(client_sock, &message[recv_len], BUFFER_LEN-1);
+			if(recv_cnt==-1) {
+                printf("read error!\n");
+                return -1;
+            }
+			recv_len+=recv_cnt;
+		}
+		
+		message[recv_len]=0;
+		printf("Message from server: %s", message);
+    }
+
+    return 0;
+}
---
+++ echo_server.cpp
@@ -0,0 +1,62 @@
+
+
+#ifndef win32
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#else
+#include <winsock2.h>
+#endif
+
+#define BUFFER_LEN 1024
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2) {
+        printf("please input port!\n");
+        return 0;
+    }
+
+    int server_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (server_sock == -1) {
+        printf("socket error!\n");
+        return -1;
+    }
+    
+    struct sockaddr_in server_addr;
+    memset(&server_addr, sizeof(server_addr), 0);
+    server_addr.sin_family=AF_INET;
+	server_addr.sin_addr.s_addr=htonl(INADDR_ANY);
+	server_addr.sin_port=htons(atoi(argv[1]));
+
+    if (bind(server_sock, (sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
+        printf("bind error!\n");
+        return -1;
+    }
+
+    if (listen(server_sock, 5) == -1) {
+        printf("listen error!\n");
+        return -1;
+    }
+
+    struct sockaddr_in client_addr;
+    socklen_t client_addr_len = sizeof(client_addr);
+    printf("wait connect,listening port: %s\n", argv[1]);
+    int client_sock = accept(server_sock, (struct sockaddr*)&client_addr, &client_addr_len);
+
+    char buffer[BUFFER_LEN];
+    int recv_len = 0;
+    while((recv_len = read(client_sock, buffer, BUFFER_LEN)) != 0) {
+        buffer[recv_len] = 0;
+        printf("recv message: %s\n", buffer);
+        write(client_sock, buffer, recv_len);
+    }
+
+    close(client_sock);
+    close(server_sock);
+
+    return 0;
+}
